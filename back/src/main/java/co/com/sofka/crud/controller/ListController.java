22778ce3e379b06b48592e179d2a6a8ab9a4f0a4
kevin alvarez <keymar1891gmail.com>
2021-06-04 21:25:01 -0500

    create entidad ListController

--- back/src/main/java/co/com/sofka/crud/controller/ListController.java
+++ back/src/main/java/co/com/sofka/crud/controller/ListController.java
@@ -1,5 +1,15 @@
 package co.com.sofka.crud.controller;
 
+import co.com.sofka.crud.services.ListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ListController {
+    @Autowired
+
+    private ListService serviceList;
 
 }
